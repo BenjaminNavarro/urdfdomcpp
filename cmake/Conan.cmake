d15e68c56d23f6fe92b81eea448f23b7ba857151
Benjamin Navarro <navarro.benjamin13@gmail.com>
2020-06-23 19:51:22 +0200

    build: Conan is now mandatory and better integrated

--- cmake/Conan.cmake
+++ cmake/Conan.cmake
@@ -1,38 +1,33 @@
-# From https://github.com/filipdutescu/modern-cpp-template
-if(${PROJECT_NAME}_ENABLE_CONAN)
-  #
-  # Setup Conan requires and options here:
-  #
-
-  set(${PROJECT_NAME}_CONAN_REQUIRES "tinyxml2/8.0.0")
-  set(${PROJECT_NAME}_CONAN_OPTIONS "")
-
-  #
-  # If `conan.cmake` (from https://github.com/conan-io/cmake-conan) does not exist, download it.
-  #
-  if(NOT EXISTS "${CMAKE_BINARY_DIR}/conan.cmake")
-    message(
-      STATUS
-        "Downloading conan.cmake from https://github.com/conan-io/cmake-conan..."
-    )
-    file(DOWNLOAD "https://github.com/conan-io/cmake-conan/raw/v0.15/conan.cmake"
-      "${CMAKE_BINARY_DIR}/conan.cmake"
-    )
-    message(STATUS "Cmake-Conan downloaded succesfully.")
-  endif()
-
-  include(${CMAKE_BINARY_DIR}/conan.cmake)
-
-  conan_cmake_run(
-    REQUIRES
-      ${${PROJECT_NAME}_CONAN_REQUIRES}
-    OPTIONS
-      ${${PROJECT_NAME}_CONAN_OPTIONS}
-    BASIC_SETUP
-      CMAKE_TARGETS # Individual targets to link to
-    BUILD
-      missing
-  )
-
-  message("Conan is setup and all requires have been installed.")
+# Automatically downloads conan.cmake if not already present
+if(NOT EXISTS "${CMAKE_BINARY_DIR}/conan.cmake")
+    message(STATUS "Downloading conan.cmake from https://github.com/conan-io/cmake-conan")
+    file(DOWNLOAD   https://github.com/conan-io/cmake-conan/raw/v0.15/conan.cmake
+                    ${CMAKE_BINARY_DIR}/conan.cmake)
 endif()
+
+include(${CMAKE_BINARY_DIR}/conan.cmake)
+  
+conan_add_remote(
+    NAME bnavarro
+    URL https://api.bintray.com/conan/benjaminnavarro/bnavarro
+)
+
+conan_add_remote(
+    NAME public-conan
+    URL https://api.bintray.com/conan/bincrafters/public-conan
+)
+
+if(ENABLE_TESTING)
+    set(conan_build_tests True)
+else()
+    set(conan_build_tests False)
+endif()
+
+conan_cmake_run(
+    CONANFILE conan/conanfile.py
+    BASIC_SETUP CMAKE_TARGETS
+    BUILD missing
+    OPTIONS urdfdomcpp:build_tests=${conan_build_tests}
+)
+
+set(conan_build_tests)
--- conan/conanfile.py
+++ conan/conanfile.py
@@ -3,32 +3,45 @@ from conans import ConanFile, CMake, tools
 
 class UrdfdomcppConan(ConanFile):
     name = "urdfdomcpp"
-    version = "0.1"
+    version = "1.0"
     license = "BSD"
     author = "Benjamin Navarro <navarro.benjamin13@gmail.com>"
     url = "https://github.com/BenjaminNavarro/urdfdomcpp"
     description = "A non-ROS version of urdfdom"
     topics = ("c++", "urdf")
     settings = "os", "compiler", "build_type", "arch"
-    options = {"shared": [True, False]}
-    default_options = {"shared": False}
+    options = {"shared": [True, False], "fPIC": [True, False], "build_tests": [True, False]}
+    default_options = {"shared": False, "fPIC": True, "build_tests": False}
     generators = "cmake"
     requires = "tinyxml2/8.0.0"
+    build_requires = "cmake/[>=3.8]"
+
+    def requirements(self):
+        if self.options.build_tests:
+            self.requires("gtest/1.10.0")
+            self.requires("cppcheck_installer/2.0@bincrafters/stable")
+
+    def configure(self):
+        if self.settings.compiler == 'Visual Studio':
+            del self.options.fPIC
 
     def source(self):
         self.run("git clone https://github.com/BenjaminNavarro/urdfdomcpp.git")
 
     def build(self):
         cmake = CMake(self)
-        cmake.definitions["urdfdomcpp_ENABLE_CONAN"] = True
-        cmake.definitions["ENABLE_TESTING"] = True
+        if self.options.build_tests:
+            cmake.definitions["ENABLE_TESTING"] = True
         cmake.configure(source_folder="urdfdomcpp")
         cmake.build()
-        cmake.test()
+        if self.options.build_tests:
+            cmake.test()
         cmake.install()
 
     def package(self):
-        self.copy("*.h", dst="include", src="urdfdomcpp/include")
+        # self.copy("*.h", dst="include", src="urdfdomcpp/include")
+        self.copy("bin/check_urdf", dst="bin", keep_path=False)
+        self.copy("bin/urdf_to_graphiz", dst="bin", keep_path=False)
         self.copy("*.lib", dst="lib", keep_path=False)
         self.copy("*.dll", dst="bin", keep_path=False)
         self.copy("*.so", dst="lib", keep_path=False)
